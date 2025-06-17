17e0e3068a264ea6f60edeffcc6f2f3d7b8d1f36
nguyentienquan408 <Nguyentienquan408@gmail.com>
2025-06-18 06:25:35 +0700

    fixbaitap

--- bai_1/bai_tap/student_management.sql
+++ bai_1/bai_tap/student_management.sql
@@ -1,30 +1,18 @@
-create database sales_management;
-use sales_management;
-create table customers(
-	c_id int ,
-    c_name varchar(50) not null,
-    c_age int not null,
-    primary key(c_id)
+create database student_management;
+use student_management;
+create table students(
+	student_id int not null,
+    student_name varchar(50) not null,
+    student_age int not null,
+    student_country varchar(150) not null
 );
-create table orders(
-	o_id int,
-    c_id int,
-    o_date date not null,
-    o_total_price double not null,
-    primary key(c_id),
-    foreign key (o_id) references customers(c_id)
+create table classes(
+	id int not null ,
+    class_name varchar(50) not null
 );
-create table products(
-	p_id int,
-    p_name varchar(50) not null,
-    p_price double not null,
-    primary key(p_id)
-);
-create table order_details(
-	o_id int,
-    p_id int,
-    od_qty varchar(50) not null,
-    primary key(o_id,p_id),
-    foreign key (o_id) references orders(o_id),
-    foreign key (p_id) references products(p_id)
+create table teacher(
+	teacher_id int not null,
+    teacher_name varchar(50) not null,
+    teacher_age int not null,
+    teacher_country varchar(150) not null
 );
